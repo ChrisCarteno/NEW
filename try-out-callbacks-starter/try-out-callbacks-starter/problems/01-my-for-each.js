634bef2c119c81e2386d6b958f30961f43ab8fd0
Chrispy <christiancarteno@hotmail.com>
2021-12-28 14:41:45 -0800

    01-myForEach

--- try-out-callbacks-starter/try-out-callbacks-starter/problems/01-my-for-each.js
+++ try-out-callbacks-starter/try-out-callbacks-starter/problems/01-my-for-each.js
@@ -6,25 +6,18 @@ element, index, and array itself. The function does not need to return any value
 Do not use the built in Array#forEach.
 
 Examples:
-
-myForEach(['a', 'b', 'c'], function (el, i) {
-    console.log(el + ' is at index ' + i);
-}); // prints
+*******************************************************************************/
+ // prints
 // a is at index 0
 // b is at index 1
 // c is at index 2
 
-let test = [];
-myForEach(['laika', 'belka'], function (el) {
-    test.push(el.toUpperCase());
-});
-console.log(test); // ['LAIKA', 'BELKA']
-*******************************************************************************/
-
-let myForEach = function() {
-
+let myForEach = function (arr, cb) {
+    for(let i = 0; i < arr.length; i++){
+        let el = arr[i];
+        cb(el, i);
+    }
 };
-
 
 
 
