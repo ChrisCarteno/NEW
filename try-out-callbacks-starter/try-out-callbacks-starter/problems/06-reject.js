4df192e3a0bf426543ac973fe9a88b3624dc1175
Jennifer Doan <jennildoan@gmail.com>
2021-12-28 16:00:51 -0800

    donee

--- try-out-callbacks-starter/try-out-callbacks-starter/problems/06-reject.js
+++ try-out-callbacks-starter/try-out-callbacks-starter/problems/06-reject.js
@@ -21,14 +21,16 @@ let hasA = function(s) {
 console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
 *******************************************************************************/
 
-let reject = function() {
-
+let reject = function (arr, cb) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let ele = cb(arr[i]);
+    if (!ele) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 };
-
-
-
-
-
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = reject;
